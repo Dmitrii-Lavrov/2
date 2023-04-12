1c72dd5022a7468ee15c299b915665020b2de3fa
Dmitrii Lavrov <amidnik@rambler.ru>
2023-04-12 21:01:18 +0300

    ok

--- lesson_3.py
+++ lesson_3.py
@@ -3,8 +3,7 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-        self.prev = None
-  
+        self.prev = None  
   
 class LinkedList: 
     
@@ -18,31 +17,58 @@ class LinkedList:
             self.head.prev = new_node        
         self.head = new_node
 
-    def reverse(self):
-        temp = None
-        current = self.head          
-        while current is not None:
-            temp = current.prev
-            current.prev = current.next
-            current.next = temp
-            current = current.prev        
-        if temp is not None:
-            self.head = temp.prev    
-  
     def printList(self, node):
         while(node is not None):
-            print(node.data)
+            print(node.data, end = ' ')
             node = node.next  
 
+def reverse(self):
+            temp = None
+            current = self.head          
+            while current is not None:
+                temp = current.prev
+                current.prev = current.next
+                current.next = temp
+                current = current.prev        
+            if temp is not None:
+                self.head = temp.prev    
+  
+    
+def bubbleSort(self):
+        swapped = 0
+        temp = None    
+        
+      
+        while True:
+            swapped = 0
+            current = self  
+            while (current.next is not temp):       
+                if (current.data > current.next.data):           
+                    current.data, current.next.data = current.next.data, current.data
+                    swapped = 1;       
+                current = current.next;       
+            temp = current;    
+            if swapped == 0:
+                break
+
+    
 
 l1 = LinkedList()
 l1.List_1(1)
 l1.List_1(2)
+l1.List_1(5)
+l1.List_1(7)
+l1.List_1(6)
+l1.List_1(9)
 l1.List_1(3)
-l1.List_1(4)
   
 print ("Заданный список")
 l1.printList(l1.head)
-l1.reverse()  
-print ("Перевернутый список")
+reverse(l1)  
+print ("\nПеревернутый список")
 l1.printList(l1.head)
+bubbleSort(l1.head)
+print ("\nОтсортированный список")
+l1.printList(l1.head)
+
+
